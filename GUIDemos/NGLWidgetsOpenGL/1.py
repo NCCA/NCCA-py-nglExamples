fa6f8196e18ab9cd439d7681106947d62729a34d
Jon Macey <jmacey@bournemouth.ac.uk>
2025-12-03 15:40:00 +0000

    NGL Widgets demo added

---
+++ GUIDemos/NGLWidgetsOpenGL/1.py
@@ -0,0 +1,237 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'MainWindow.ui'
+##
+## Created by: Qt User Interface Compiler version 6.10.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (
+    QCoreApplication,
+    QDate,
+    QDateTime,
+    QLocale,
+    QMetaObject,
+    QObject,
+    QPoint,
+    QRect,
+    QSize,
+    Qt,
+    QTime,
+    QUrl,
+)
+from PySide6.QtGui import (
+    QBrush,
+    QColor,
+    QConicalGradient,
+    QCursor,
+    QFont,
+    QFontDatabase,
+    QGradient,
+    QIcon,
+    QImage,
+    QKeySequence,
+    QLinearGradient,
+    QPainter,
+    QPalette,
+    QPixmap,
+    QRadialGradient,
+    QTransform,
+)
+from PySide6.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QComboBox,
+    QDoubleSpinBox,
+    QFrame,
+    QGridLayout,
+    QGroupBox,
+    QLabel,
+    QPushButton,
+    QSizePolicy,
+    QSpacerItem,
+    QWidget,
+)
+from rgbcolourwidget import RGBColourWidget
+
+
+class Ui_MainWidget(object):
+    def setupUi(self, MainWidget):
+        if not MainWidget.objectName():
+            MainWidget.setObjectName("MainWidget")
+        MainWidget.resize(1024, 720)
+        self.main_window_grid_layout = QGridLayout(MainWidget)
+        self.main_window_grid_layout.setObjectName("main_window_grid_layout")
+        self.transform_gb = QGroupBox(MainWidget)
+        self.transform_gb.setObjectName("transform_gb")
+        self.gridLayout = QGridLayout(self.transform_gb)
+        self.gridLayout.setObjectName("gridLayout")
+        self.label = QLabel(self.transform_gb)
+        self.label.setObjectName("label")
+
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+
+        self.rotation_x = QDoubleSpinBox(self.transform_gb)
+        self.rotation_x.setObjectName("rotation_x")
+        self.rotation_x.setMinimum(-360.000000000000000)
+        self.rotation_x.setMaximum(360.000000000000000)
+
+        self.gridLayout.addWidget(self.rotation_x, 1, 0, 1, 1)
+
+        self.rotation_y = QDoubleSpinBox(self.transform_gb)
+        self.rotation_y.setObjectName("rotation_y")
+        self.rotation_y.setMinimum(-360.000000000000000)
+        self.rotation_y.setMaximum(360.000000000000000)
+
+        self.gridLayout.addWidget(self.rotation_y, 1, 1, 1, 1)
+
+        self.rotation_z = QDoubleSpinBox(self.transform_gb)
+        self.rotation_z.setObjectName("rotation_z")
+        self.rotation_z.setMinimum(-360.000000000000000)
+        self.rotation_z.setMaximum(360.000000000000000)
+
+        self.gridLayout.addWidget(self.rotation_z, 1, 2, 1, 1)
+
+        self.label_2 = QLabel(self.transform_gb)
+        self.label_2.setObjectName("label_2")
+
+        self.gridLayout.addWidget(self.label_2, 2, 0, 1, 1)
+
+        self.scale_x = QDoubleSpinBox(self.transform_gb)
+        self.scale_x.setObjectName("scale_x")
+        self.scale_x.setMinimum(-4.000000000000000)
+        self.scale_x.setMaximum(4.000000000000000)
+        self.scale_x.setSingleStep(0.010000000000000)
+        self.scale_x.setValue(1.000000000000000)
+
+        self.gridLayout.addWidget(self.scale_x, 3, 0, 1, 1)
+
+        self.scale_y = QDoubleSpinBox(self.transform_gb)
+        self.scale_y.setObjectName("scale_y")
+        self.scale_y.setMinimum(-4.000000000000000)
+        self.scale_y.setMaximum(4.000000000000000)
+        self.scale_y.setSingleStep(0.010000000000000)
+        self.scale_y.setValue(1.000000000000000)
+
+        self.gridLayout.addWidget(self.scale_y, 3, 1, 1, 1)
+
+        self.scale_z = QDoubleSpinBox(self.transform_gb)
+        self.scale_z.setObjectName("scale_z")
+        self.scale_z.setMinimum(-4.000000000000000)
+        self.scale_z.setMaximum(4.000000000000000)
+        self.scale_z.setSingleStep(0.010000000000000)
+        self.scale_z.setValue(1.000000000000000)
+
+        self.gridLayout.addWidget(self.scale_z, 3, 2, 1, 1)
+
+        self.label_3 = QLabel(self.transform_gb)
+        self.label_3.setObjectName("label_3")
+
+        self.gridLayout.addWidget(self.label_3, 4, 0, 1, 1)
+
+        self.position_x = QDoubleSpinBox(self.transform_gb)
+        self.position_x.setObjectName("position_x")
+        self.position_x.setMinimum(-20.000000000000000)
+        self.position_x.setMaximum(20.000000000000000)
+        self.position_x.setSingleStep(0.010000000000000)
+
+        self.gridLayout.addWidget(self.position_x, 5, 0, 1, 1)
+
+        self.position_y = QDoubleSpinBox(self.transform_gb)
+        self.position_y.setObjectName("position_y")
+        self.position_y.setMinimum(-20.000000000000000)
+        self.position_y.setMaximum(20.000000000000000)
+        self.position_y.setSingleStep(0.010000000000000)
+
+        self.gridLayout.addWidget(self.position_y, 5, 1, 1, 1)
+
+        self.position_z = QDoubleSpinBox(self.transform_gb)
+        self.position_z.setObjectName("position_z")
+        self.position_z.setMinimum(-20.000000000000000)
+        self.position_z.setMaximum(20.000000000000000)
+        self.position_z.setSingleStep(0.010000000000000)
+
+        self.gridLayout.addWidget(self.position_z, 5, 2, 1, 1)
+
+        self.main_window_grid_layout.addWidget(self.transform_gb, 0, 1, 1, 1)
+
+        self.draw_gb = QGroupBox(MainWidget)
+        self.draw_gb.setObjectName("draw_gb")
+        self.draw_layout = QGridLayout(self.draw_gb)
+        self.draw_layout.setObjectName("draw_layout")
+        self.colour_button = QPushButton(self.draw_gb)
+        self.colour_button.setObjectName("colour_button")
+
+        self.draw_layout.addWidget(self.colour_button, 3, 0, 1, 1)
+
+        self.verticalSpacer = QSpacerItem(
+            20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding
+        )
+
+        self.draw_layout.addItem(self.verticalSpacer, 5, 0, 1, 1)
+
+        self.object_selection = QComboBox(self.draw_gb)
+        self.object_selection.addItem("")
+        self.object_selection.addItem("")
+        self.object_selection.addItem("")
+        self.object_selection.setObjectName("object_selection")
+
+        self.draw_layout.addWidget(self.object_selection, 0, 0, 1, 1)
+
+        self.wireframe = QCheckBox(self.draw_gb)
+        self.wireframe.setObjectName("wireframe")
+
+        self.draw_layout.addWidget(self.wireframe, 1, 0, 1, 1)
+
+        self.frame = RGBColourWidget(self.draw_gb)
+        self.frame.setObjectName("frame")
+        self.frame.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Shadow.Raised)
+
+        self.draw_layout.addWidget(self.frame, 4, 0, 1, 1)
+
+        self.main_window_grid_layout.addWidget(self.draw_gb, 1, 1, 1, 1)
+
+        self.horizontalSpacer = QSpacerItem(
+            40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
+        )
+
+        self.main_window_grid_layout.addItem(self.horizontalSpacer, 0, 0, 1, 1)
+
+        self.retranslateUi(MainWidget)
+
+        QMetaObject.connectSlotsByName(MainWidget)
+
+    # setupUi
+
+    def retranslateUi(self, MainWidget):
+        MainWidget.setWindowTitle(
+            QCoreApplication.translate("MainWidget", "ngl Qt Demo", None)
+        )
+        self.transform_gb.setTitle(
+            QCoreApplication.translate("MainWidget", "Transform", None)
+        )
+        self.label.setText(QCoreApplication.translate("MainWidget", "Rotation", None))
+        self.label_2.setText(QCoreApplication.translate("MainWidget", "Scale", None))
+        self.label_3.setText(QCoreApplication.translate("MainWidget", "Position", None))
+        self.draw_gb.setTitle(QCoreApplication.translate("MainWidget", "Draw", None))
+        self.colour_button.setText(
+            QCoreApplication.translate("MainWidget", "Choose Colour", None)
+        )
+        self.object_selection.setItemText(
+            0, QCoreApplication.translate("MainWidget", "Teapot", None)
+        )
+        self.object_selection.setItemText(
+            1, QCoreApplication.translate("MainWidget", "Sphere", None)
+        )
+        self.object_selection.setItemText(
+            2, QCoreApplication.translate("MainWidget", "Cube", None)
+        )
+
+        self.wireframe.setText(
+            QCoreApplication.translate("MainWidget", "WireFrame", None)
+        )
+
+    # retranslateUi
---
+++ GUIDemos/NGLWidgetsOpenGL/main.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env -S uv run  --script
+
+import sys
+
+from ncca.ngl import Vec3
+from ncca.ngl.widgets import LookAtWidget, RGBColourWidget, TransformWidget
+from PyNGLScene import PyNGLScene
+from PySide6.QtCore import QFile, Qt, Signal
+from PySide6.QtGui import QKeyEvent, QSurfaceFormat
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QApplication, QColorDialog, QMainWindow, QWidget
+
+
+class Loader(QUiLoader):
+    def createWidget(self, class_name, parent=None, name=""):
+        if class_name == "RGBColourWidget":
+            return RGBColourWidget(parent)
+        elif class_name == "TransformWidget":
+            return TransformWidget(parent)
+        elif class_name == "LookAtWidget":
+            return LookAtWidget(parent)
+        return super().createWidget(class_name, parent, name)
+
+
+class MainWindow(QMainWindow):
+    """
+    The main window of the application, which hosts the OpenGL scene and UI controls.
+
+    This class loads the user interface from a .ui file, integrates the PyNGLScene
+    OpenGL widget, and connects UI signals (e.g., button clicks, slider changes)
+    to the corresponding slots in the OpenGL scene to manipulate the 3D object.
+
+    Attributes:
+        colour_update (Signal): A signal that emits RGB float values when a new color is selected.
+        scene (PyNGLScene): The OpenGL widget where the 3D scene is rendered.
+    """
+
+    # signal to emit when the colour is changed
+    # colour_update = Signal(float, float, float)
+
+    def __init__(self) -> None:
+        """Initialize the MainWindow with UI setup and configuration loading."""
+        super().__init__()
+
+        self.load_ui()
+        # Setup the custom promoted widgets
+        self.colour.set_name("Colour")
+        self.colour.set_colour(Vec3(1.0, 1.0, 0.0))
+        self.lookat_widget.set_eye(Vec3(0.0, 1.0, 4.0))
+        self.lookat_widget.set_name("Look At")
+        self.transform_widget.set_name("Model Transform")
+        self.scene = PyNGLScene()
+        self.centralWidget().layout().addWidget(self.scene, 0, 0, 6, 1)
+
+        self.resize(1024, 720)
+        self._connect_slots()
+
+    def _connect_slots(self) -> None:
+        """Connect UI element signals to their corresponding slots."""
+        self.wireframe.toggled.connect(self.scene.set_wireframe)
+        self.object_selection.currentTextChanged.connect(self.scene.set_model_name)
+        self.transform_widget.valueChanged.connect(self.scene.set_transform)
+        self.lookat_widget.valueChanged.connect(self.scene.set_camera)
+        self.colour.colourChanged.connect(self.scene.set_colour)
+        self.fov.valueChanged.connect(self.update_perspective)
+        self.near.valueChanged.connect(self.update_perspective)
+        self.far.valueChanged.connect(self.update_perspective)
+
+    def update_perspective(self) -> None:
+        """Update the perspective projection matrix."""
+        self.scene.update_perspective(
+            self.fov.value(), self.near.value(), self.far.value()
+        )
+        self.update()
+
+    def load_ui(self) -> None:
+        """Load the UI from a .ui file and set up the connections."""
+        try:
+            loader = Loader()
+            ui_file = QFile("MainWindow.ui")
+            ui_file.open(QFile.ReadOnly)
+
+            loaded_ui = loader.load(ui_file, self)
+            self.setCentralWidget(loaded_ui)
+
+            # Add all children with object names as attributes
+            for child in loaded_ui.findChildren(QWidget):
+                name = child.objectName()
+                if name:
+                    setattr(self, name, child)
+
+            ui_file.close()
+
+        except Exception as e:
+            print(f"Error loading UI file: {e}")
+            raise
+
+    def keyPressEvent(self, event: QKeyEvent) -> None:
+        """Handle key press events to close the window on Escape."""
+        if event.key() == Qt.Key_Escape:
+            self.close()
+
+
+def main():
+    """Main application entry point."""
+
+    app = QApplication(sys.argv)
+    # Create a QSurfaceFormat object to request a specific OpenGL context
+    format: QSurfaceFormat = QSurfaceFormat()
+    # Request 4x multisampling for anti-aliasing
+    format.setSamples(4)
+    # Request OpenGL version 4.1 as this is the highest supported on macOS
+    format.setMajorVersion(4)
+    format.setMinorVersion(1)
+    # Request a Core Profile context, which removes deprecated, fixed-function pipeline features
+    format.setProfile(QSurfaceFormat.CoreProfile)
+    # Request a 24-bit depth buffer for proper 3D sorting
+    format.setDepthBufferSize(24)
+    # Set default format for all new OpenGL contexts
+    QSurfaceFormat.setDefaultFormat(format)
+
+    # Apply this format to all new OpenGL contexts
+    QSurfaceFormat.setDefaultFormat(format)
+
+    try:
+        window = MainWindow()
+        window.show()
+        sys.exit(app.exec())
+    except Exception as e:
+        print(f"Application error: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
