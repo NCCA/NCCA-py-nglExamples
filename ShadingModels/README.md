0abfa41b13bf104dbdf087a28cbd18e5b499d262
Jon Macey <jmacey@bournemouth.ac.uk>
2025-12-08 20:44:59 +0000

    added README.md and image

---
+++ ShadingModels/README.md
@@ -0,0 +1,6 @@
+## Shading Models
+
+This tool is designed to demonstrate various shading models in computer graphics and is used as part of my lectures.
+
+
+The tool loads in a json file containing the shader description and material properties for each model.
--- ShadingModels/shaders/TwoSideVert.glsl
+++ ShadingModels/shaders/TwoSideVert.glsl
@@ -14,27 +14,27 @@ uniform mat4 MV;
 
 struct LightInfo
 {
-	// Light position in eye coords.
-	vec3 position;
-	// Ambient light intensity
-	vec3 La;
-	// Diffuse light intensity
-	vec3 Ld;
-	// Specular light intensity
-	vec3 Ls;
+    // Light position in eye coords.
+    vec3 position;
+    // Ambient light intensity
+    vec3 La;
+    // Diffuse light intensity
+    vec3 Ld;
+    // Specular light intensity
+    vec3 Ls;
 };
 uniform LightInfo light;
 
 struct MaterialInfo
 {
-	// Ambient reflectivity
-	vec3 Ka;
-	// Diffuse reflectivity
-	vec3 Kd;
-	// Specular reflectivity
-	vec3 Ks;
-	// Specular shininess factor
-	float shininess;
+    // Ambient reflectivity
+    vec3 Ka;
+    // Diffuse reflectivity
+    vec3 Kd;
+    // Specular reflectivity
+    vec3 Ks;
+    // Specular shininess factor
+    float shininess;
 };
 
 uniform MaterialInfo material;
@@ -52,29 +52,29 @@ void getEyeSpace(out vec3 norm, out vec4 position)
 
 vec3 phongModel( vec4 position, vec3 norm )
 {
-	vec3 s = normalize(vec3(light.position - position.xyz));
-	vec3 v = normalize(-position.xyz);
-	vec3 r = reflect( -s, norm );
-	vec3 ambient = light.La * material.Ka;
-	float sDotN = max( dot(s,norm), 0.0 );
-	vec3 diffuse = light.Ld * material.Kd * sDotN;
-	vec3 spec = vec3(0.0);
-	if( sDotN > 0.0 )
-	{
-		spec = light.Ls * material.Ks * pow( max( dot(r,v), 0.0 ), material.shininess );
-	}
-	return ambient + diffuse + spec;
+    vec3 s = normalize(vec3(light.position - position.xyz));
+    vec3 v = normalize(-position.xyz);
+    vec3 r = reflect( -s, norm );
+    vec3 ambient = light.La * material.Ka;
+    float sDotN = max( dot(s,norm), 0.0 );
+    vec3 diffuse = light.Ld * material.Kd * sDotN;
+    vec3 spec = vec3(0.0);
+    if( sDotN > 0.0 )
+    {
+    	spec = light.Ls * material.Ks * pow( max( dot(r,v), 0.0 ), material.shininess );
+    }
+    return ambient + diffuse + spec;
 }
 
 void main()
 {
-	vec3 eyeNorm;
-	vec4 eyePosition;
-	// Get the position and normal in eye space
-	getEyeSpace(eyeNorm, eyePosition);
-	// Evaluate the lighting equation.
-	frontColour = phongModel( eyePosition, eyeNorm );
-	backColour = phongModel(eyePosition, -eyeNorm);
-	// Convert position to clip coordinates and pass along
-	gl_Position = MVP*vec4(inVert,1.0);
+    vec3 eyeNorm;
+    vec4 eyePosition;
+    // Get the position and normal in eye space
+    getEyeSpace(eyeNorm, eyePosition);
+    // Evaluate the lighting equation.
+    frontColour = phongModel( eyePosition, eyeNorm );
+    backColour = phongModel(eyePosition, -eyeNorm);
+    // Convert position to clip coordinates and pass along
+    gl_Position = MVP*vec4(inVert,1.0);
 }
