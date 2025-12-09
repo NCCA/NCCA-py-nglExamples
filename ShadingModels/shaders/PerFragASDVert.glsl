72973115d0c14d759a96a44ee8797ee1639985b9
Jon Macey <jmacey@bournemouth.ac.uk>
2025-12-09 12:39:37 +0000

    added more shaders

---
+++ ShadingModels/shaders/PerFragASDVert.glsl
@@ -0,0 +1,25 @@
+#version 330 core
+// this shader is modified from the OpenGL Shading Language cookbook
+/// @brief the vertex passed in
+layout (location =0) in vec3 inVert;
+/// @brief the normal passed in
+layout (location =1) in vec3 inNormal;
+/// @brief the in uv
+layout( location=2)in vec2 inUV;
+
+// transform matrix values
+uniform mat4 MVP;
+uniform mat3 normal_matrix;
+uniform mat4 MV;
+
+out vec3 position;
+out vec3 normal;
+
+void main()
+{
+	// Convert normal and position to eye coords
+	 normal = normalize( normal_matrix * inNormal);
+	 position = vec3(MV * vec4(inVert,1.0));
+	// Convert position to clip coordinates and pass along
+	gl_Position = MVP*vec4(inVert,1.0);
+}
